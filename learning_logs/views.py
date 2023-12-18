0fd682defd1282f62503c11c2cf19c507adb1cc2
Kevin Menpara <202101079@daiict.ac.in>
2023-12-18 16:06:58 +0530

    Error Handling

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -22,6 +22,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
+    topic = get_object_or_404(Topic, id=topic_id)
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
     if topic.owner != request.user:
@@ -85,3 +86,9 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def custom_404(request, exception=None):
+    return render(request, '../learning_log/templates/404.html', status=404)
+
+def custom_500(request):
+    return render(request, '../learning_log/templates/500.html', status=500)
