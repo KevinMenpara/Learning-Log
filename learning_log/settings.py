00eb495214083ae63883c5ae9d53e74ebc1c4a45
Kevin Menpara <202101079@daiict.ac.in>
2023-12-18 15:12:02 +0530

    Styling with Django.bootsrap3

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party apps
+    'bootstrap3',
     # My apps
     'learning_logs',
     #My Users
@@ -128,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My settings
 LOGIN_URL = '/users/login/'
+
+# Settings for django-bootstrap3
+BOOTSTRAP3 = {
+ 'include_jquery': True,
+ }
